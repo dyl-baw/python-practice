73e3f6c0d6cdc3a5e021dca7305021e00dca1787
dyl-baw <dvd.y.lee@gmail.com>
2023-01-07 11:06:17 -0800

    Enhanced UI

--- scripts/convert_string_title_case.py
+++ scripts/convert_string_title_case.py
@@ -1,6 +1,8 @@
 def format_names(f_name, l_name):
-    name = (f'{f_name} {l_name}').title()
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    name = (f'Result: {f_name} {l_name}').title()
     return name
 
-print(format_names("david", "DAVID"))
+print(format_names(input("What is your first name? "), input("What is your last name? ")))
 
