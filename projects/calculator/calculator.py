f9b45c02f848f769bfdb131f02ef520e122e1ea2
David Lee <dvd.y.lee@gmail.com>
2023-01-14 01:11:10 -0800

    fixed up calculator.py

--- projects/calculator/calculator.py
+++ projects/calculator/calculator.py
@@ -1,6 +1,6 @@
 from calculator_art import logo
 
-print(logo)
+
 
 
 
@@ -17,17 +17,42 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-operations = {'+': add, '-': subtract, '*': multiply, '/' :divide}
+operations = {
+    '+': add,
+    '-': subtract, 
+    '*': multiply,
+    '/' :divide
+}
 
-first_number = int(input("What is the first number?: "))
 
-for symbols in operations:
-    print(symbols)
 
-pick_operation = input("Select an Operation from the line above: ")
-second_number = int(input("What is the second number?: "))
+calculate = True
+def calculator():
+    print(logo)
+    first_number = float(input("What is the first number?: "))
 
-calculation_function = operations[pick_operation]
-answer = calculation_function(first_number, second_number)
+    for symbols in operations:
+        print(symbols)
+    calculate = True
+    while calculate:
+        pick_operation = input("Select an Operation from the line above: ")
+        second_number = float(input("What is the second number?: "))
 
-print(f"{first_number} {pick_operation} {second_number} = {answer}")
+        calculation_function = operations[pick_operation]
+        first_answer = calculation_function(first_number, second_number)
+
+        print(f"{first_number} {pick_operation} {second_number} = {first_answer}")
+
+        if input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to exit.: ") == "y":
+            first_number = first_answer
+        else:
+            calculate = False
+            exit()
+
+calculator()
+# operation_symbol = input("Pick another operation: ")
+# third_number = int(input("What's the next number?: "))
+# calculation_function = operations[operation_symbol]
+# second_answer = calculation_function(first_answer, third_number)
+
+# print(f"{first_answer} {operation_symbol} {third_number} = {second_answer}")
